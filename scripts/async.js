6247c2587f83cc2d0bd08594b2138b4c818d8bac
Thorsten Lorenz <thlorenz@gmx.de>
2021-02-22 15:13:08 -0500

    scripts: async

---
+++ scripts/async.js
@@ -0,0 +1,27 @@
+'use strict'
+const { getFiles } = require('../lib/util')
+const fs = require('fs')
+
+const files = getFiles()
+
+console.time('read:async')
+
+let contentLen = 0
+let tasks = files.length
+
+for (const file of files) {
+  fs.readFile(file, 'utf8', function (err, content) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    contentLen += content.length
+    tasks--
+    if (tasks == 0) done()
+  })
+}
+
+function done() {
+  console.timeEnd('read:async')
+  console.log('Total lines: %d', contentLen)
+}
